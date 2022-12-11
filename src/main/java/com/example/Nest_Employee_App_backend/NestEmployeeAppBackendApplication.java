7ee51a6cf041ae6c4cf483d10bdd2dffcd9a6b42
rahulg3520 <rahul@nestgroup.net>
2022-12-11 12:31:50 +0530

    employee app backend

---
+++ src/main/java/com/example/Nest_Employee_App_backend/NestEmployeeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nest_Employee_App_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestEmployeeAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestEmployeeAppBackendApplication.class,args);
+	}
+
+}
---
+++ src/test/java/com/example/Nest_Employee_App_backend/NestEmployeeAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nest_Employee_App_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestEmployeeAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
